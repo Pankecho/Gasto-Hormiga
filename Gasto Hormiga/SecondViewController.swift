7046faad0dd8236a15c7c3df2b62d9a6b30212b3
Juan Pablo <esponja.014@gmail.com>
2017-10-25 09:27:42 -0500

    Initial Commit

---
+++ Gasto Hormiga/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Gasto Hormiga
+//
+//  Created by Juan Pablo Martinez Ruiz on 25/10/17.
+//  Copyright © 2017 Juan Pablo Martinez Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
